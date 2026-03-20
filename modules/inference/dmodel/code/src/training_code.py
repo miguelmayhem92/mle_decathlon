ff31be5c9701c9648e619ef7f15e2bd5ab6b4aff
miguel <miguelmayhem92@gmail.com>
2026-03-20 18:02:28 +0100

    inference done

---
+++ modules/inference/dmodel/code/src/training_code.py
@@ -0,0 +1,121 @@
+import os
+from pathlib import Path
+
+import yaml
+import pandas as pd
+from loguru import logger
+import mlflow
+
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import GradientBoostingRegressor
+
+from src.custom_transformers import CustomPreprocressing
+
+DATA_FOLDER = "data"
+EXPERIMENT_NAME="forecasting"
+RUN_NAME="run_0"
+BASE_DIR = os.path.dirname("app")
+# mlflow.set_tracking_uri("http://localhost:5000/") # for local testing
+
+class TrainerClient:
+    """
+    this class contains the main steps for ml traning
+    """
+    def __init__(self,model_name:str)->None:
+        self.model_name = model_name
+        self.configs = yaml.safe_load(Path(os.path.join("configs","training.yml")).read_text())
+        self.preprocessing_configs = self.configs["training"]["preprosessing"]
+        self.features_config = self.configs["training"]["features"]
+
+    def run(self):
+        self._extraction_job()
+        self._data_processing()
+        self._get_model_definition()
+        self._fit_model()
+        self._evaluation()
+        self._log_model()
+
+
+    def _extraction_job(self):
+        """
+        this method extracts the data from the warehouse
+        """
+        logger.info("Launching data extraction")
+        self.df_bu_feat = pd.read_csv(os.path.join(DATA_FOLDER,"bu_feat.csv.gz")) 
+        self.df_train = pd.read_csv(os.path.join(DATA_FOLDER, "train.csv.gz"))
+        self.df_test = pd.read_csv(os.path.join(DATA_FOLDER, "test.csv.gz"))
+        logger.info("Data is extracted")
+
+    def _data_processing(self):
+        """
+        this method executes data preprocessing
+        """
+        logger.info("Data prerpocessing")
+        year_split = self.preprocessing_configs["year_split"]
+        # mergin the data
+        df_train_feat = pd.merge(self.df_train, self.df_bu_feat, how="left", on = "but_num_business_unit")
+        df_test_feat = pd.merge(self.df_test, self.df_bu_feat, how="left", on = "but_num_business_unit")
+
+        # Train and val set
+        df_train_feat["day_id"] = pd.to_datetime(df_train_feat["day_id"])
+        df_train_feat["day_id_week"] = df_train_feat.day_id.dt.isocalendar().week
+        df_train_feat["day_id_month"] = df_train_feat["day_id"].dt.month
+        df_train_feat["day_id_year"] = df_train_feat["day_id"].dt.year
+
+        # 2017 must a variable
+        self.df_train = df_train_feat[(df_train_feat.day_id_year < year_split)]
+        self.df_val = df_train_feat[(df_train_feat.day_id_year == year_split)]
+
+        self.y_train = self.df_train.turnover
+        self.y_val = self.df_val.turnover
+        logger.info("Data preprosessing is finished")
+
+
+    def _get_model_definition(self):
+        """
+        this method defines model architecture
+        """
+        logger.info("getting model definition")
+        num_attrib = self.features_config["numeric_attributes"]
+        cat_attrib = self.features_config["categorical_atributes"]
+        logger.info("Definig model architecture")
+        num_pipeline = Pipeline([
+            ('std_scaler', StandardScaler()),
+        ])
+        cat_onehot_pipeline = Pipeline([
+            ('encoder', OneHotEncoder(handle_unknown="ignore")),
+        ])
+        preparation_pipeline = ColumnTransformer([
+            ("num",num_pipeline, num_attrib),
+            ("cat_onehot", cat_onehot_pipeline, cat_attrib)
+        ])
+
+        self.model_pipeline = Pipeline([
+            ('preprocessing', CustomPreprocressing(cat_cols=cat_attrib )),
+            ('preparation', preparation_pipeline),
+            ('model', GradientBoostingRegressor())
+        ])
+
+    def _fit_model(self):
+        logger.info("fiting model definition")
+        self.model_pipeline.fit(self.df_train, self.y_train)
+    
+    def _evaluation(self):
+        logger.info("model evaluation")
+        y_predict_val = self.model_pipeline.predict(self.df_val)
+        self.metric_mae = mean_absolute_error(self.y_val, y_predict_val)
+
+    def _log_model(self):
+        logger.info("logging artifacts in remote repository")
+        mlflow.set_experiment(EXPERIMENT_NAME)
+        with mlflow.start_run(run_name=RUN_NAME):
+            mlflow.log_metric("val_mae", self.metric_mae)
+            mlflow.log_dict(self.configs, "configs.json")
+            mlflow.sklearn.log_model(self.model_pipeline, name="model",code_paths=["src/"])
+            mlflow.log_artifact("data/", "feature_store/")
+        logger.info("logging finished")
+
--- modules/inference/main.py
+++ modules/inference/main.py
@@ -1,10 +1,29 @@
 import os
 
-import loguru
+import pandas as pd
+from loguru import logger
 import mlflow
 
 
-def handler(event:list,context:None)->list[dict]:
+
+
+def handler(event:dict,context:None)->list[dict]:
+    """
+    executable for inferences
+
+    params:
+    event: input data as dictionary e.g. {"col1":list(), "col2":list()}
+    """
+    logger.info("transforming incoming messages")
+    input = pd.DataFrame(event)
+    input["day_id"] = pd.to_datetime(input["day_id"])
+    bu_features = pd.read_csv(os.path.join("dep_features","bu_feat.csv.gz"))
+    merged_input = pd.merge(input, bu_features, how="left", on = "but_num_business_unit")
+
+    logger.info("produccing prediction")
     model = mlflow.sklearn.load_model("model")
-    print(model)
-    return {"status":200}
+    predictions = model.predict(merged_input)
+    predictions = list(predictions)
+
+    logger.info("sending output")
+    return {"status":200, "output":predictions}
